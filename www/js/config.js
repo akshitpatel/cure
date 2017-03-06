7d0cca634991b1551f04260bce75e45cfbc4a732
Akshit Patel <jodow@ymail.com>
2017-03-07 01:57:30 +0530

    First commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={		
+	'ApiUrl':"http://app.jennyinfocom.com/kartero/api",					
+	'DialogDefaultTitle':"Cure4less",	
+	'PushProjectID':"884022571553",	
+	'APIHasKey':""
+};
